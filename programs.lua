a1b51ba22416a7ea728ecc9e77740a69ca3dd33f
wolfixxz <wolfixxz@proton.me>
2026-08-01 21:09:47 -0700

    Splited configs
    yes

---
+++ programs.lua
@@ -0,0 +1,9 @@
+
+---------------------
+---- MY PROGRAMS ----
+---------------------
+
+-- Set programs that you use
+local terminal    = "kitty"
+local fileManager = "dolphin"
+local menu        = "hyprlauncher"
